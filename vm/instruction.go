6ecf3cd41cee4299800adf625452d5d9016b76d9
czn <iamczn.cpp@gmail.com>
2019-01-05 22:29:51 +0800

    fix typo i -> inst

--- vm/instruction.go
+++ vm/instruction.go
@@ -11,48 +11,48 @@ const (
 )
 
 // Opcode extracts opcode from instruction
-func (i Instruction) Opcode() int {
-	return int(i & 0x3F)
+func (inst Instruction) Opcode() int {
+	return int(inst & 0x3F)
 }
 
 // ABC extracts A, B, C operands from instruction
-func (i Instruction) ABC() (a, b, c int) {
-	a = int((i >> 6) & 0xFF)
-	c = int((i >> 14) & 0x1FF)
-	b = int((i >> 23) & 0x1FF)
+func (inst Instruction) ABC() (a, b, c int) {
+	a = int((inst >> 6) & 0xFF)
+	c = int((inst >> 14) & 0x1FF)
+	b = int((inst >> 23) & 0x1FF)
 	return
 }
 
 // ABx extracts A, Bx operands from instruction
-func (i Instruction) ABx() (a, bx int) {
+func (inst Instruction) ABx() (a, bx int) {
 	//  Bx 0               131071              1<<18-1
 	// sBx -131071           0               (1<<18-1)<<1
 	//     |-----------------|---------------------|
 
-	a = int((i >> 6) & 0xFF)
-	bx = int((i >> 14))
+	a = int((inst >> 6) & 0xFF)
+	bx = int((inst >> 14))
 	return
 }
 
 // AsBx extracts A, sBx operands from instruction
-func (i Instruction) AsBx() (a, sBx int) {
-	a, bx := i.ABx()
+func (inst Instruction) AsBx() (a, sBx int) {
+	a, bx := inst.ABx()
 	sBx = bx - maxArgSBx
 	return
 }
 
 // Ax extracts Ax operand from instruction
-func (i Instruction) Ax() (ax int) {
-	ax = int(i >> 6)
+func (inst Instruction) Ax() (ax int) {
+	ax = int(inst >> 6)
 	return
 }
 
 // Execute an instruction
-func (i Instruction) Execute(vm api.ILuaVM) {
-	action := opcodes[i.Opcode()].action
+func (inst Instruction) Execute(vm api.ILuaVM) {
+	action := opcodes[inst.Opcode()].action
 	if action != nil {
-		action(i, vm)
+		action(inst, vm)
 	} else {
-		panic(i.OpName())
+		panic(inst.OpName())
 	}
 }
