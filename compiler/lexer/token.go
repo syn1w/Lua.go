907624e096ae49535a5dc04c3888a33e34ec0bf8
czn <iamczn.cpp@gmail.com>
2019-01-21 21:44:57 +0800

    add lexer token

---
+++ compiler/lexer/token.go
@@ -0,0 +1,93 @@
+package lexer
+
+// token kind
+const (
+	TokenEOF        = iota         // EOF
+	TokenVararg                    // ...
+	TokenSepSemi                   // ;
+	TokenSepComma                  // ,
+	TokenSepDot                    // .
+	TokenSepColon                  // :
+	TokenSepLabel                  // ::
+	TokenSepLparen                 // (
+	TokenSepRparen                 // )
+	TokenSepLBrack                 // [
+	TokenSepRBrack                 // ]
+	TokenSepLcurly                 // {
+	TokenSepRcurly                 // }
+	TokenOpAssign                  // =
+	TokenOpMinus                   // -(unm or sub)
+	TokenOpWave                    // ~(not or bxor)
+	TokenOpAdd                     // +
+	TokenOpMul                     // *
+	TokenOpDiv                     // /
+	TokenOpIDiv                    // //
+	TokenOpMod                     // %
+	TokenOpPow                     // ^
+	TokenOpBand                    // &
+	TokenOpBor                     // |
+	TokenOpShr                     // >>
+	TokenOpShl                     // <<
+	TokenOpConcat                  // ..
+	TokenOpLt                      // <
+	TokenOpLe                      // <=
+	TokenOpGt                      // >
+	TokenOpGe                      // >=
+	TokenOpEq                      // ==
+	TokenOpNe                      // ~=
+	TokenOpLen                     // #
+	TokenOpAnd                     // and
+	TokenOpOr                      // or
+	TokenOpNot                     // not
+	TokenKwBreak                   // break
+	TokenKwDo                      // do
+	TokenKwElse                    // else
+	TokenKwElseif                  // elseif
+	TokenKwEnd                     // end
+	TokenKwFalse                   // false
+	TokenKwFor                     // for
+	TokenKwFunction                // function
+	TokenKwGoto                    // goto
+	TokenKwIf                      // if
+	TokenKwIn                      // in
+	TokenKwLocal                   // local
+	TokenKwNil                     // nil
+	TokenKwRepeat                  // repeat
+	TokenKwReturn                  // return
+	TokenKwThen                    // then
+	TokenKwTrue                    // true
+	TokenKwUntil                   // until
+	TokenKwWhile                   // while
+	TokenIdentifier                // identifier
+	TokenNumber                    // number literal
+	TokenString                    // string literal
+	TokenOpUnm      = TokenOpMinus // unary minus
+	TokenOpSub      = TokenOpMinus // binary minus
+	TokenOpBnot     = TokenOpWave  // not
+	TokenOpBxor     = TokenOpWave  // xor
+)
+
+var keywars = map[string]int{
+	"and":      TokenOpAnd,
+	"break":    TokenKwBreak,
+	"do":       TokenKwDo,
+	"else":     TokenKwElse,
+	"elseif":   TokenKwElseif,
+	"end":      TokenKwEnd,
+	"false":    TokenKwFalse,
+	"for":      TokenKwFor,
+	"function": TokenKwFunction,
+	"goto":     TokenKwGoto,
+	"if":       TokenKwIf,
+	"in":       TokenKwIn,
+	"local":    TokenKwLocal,
+	"nil":      TokenKwNil,
+	"not":      TokenOpNot,
+	"or":       TokenOpOr,
+	"repeat":   TokenKwRepeat,
+	"return":   TokenKwReturn,
+	"then":     TokenKwThen,
+	"true":     TokenKwTrue,
+	"untile":   TokenKwUntil,
+	"while":    TokenKwWhile,
+}
